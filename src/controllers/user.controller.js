8081d1048ad29d76e24d31fdfadbb6b3a2cf9e7e
SyedAliHassanNaqvi <142111910+SyedAliHassanNaqvi@users.noreply.github.com>
2025-02-06 19:54:06 +0500

    add user account update detail endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -239,5 +239,105 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
 })
 // in case of mobile app where cookies are not available, we wrote in OR : req.body.refreshToken
 
+const changeCurrentPassword = asyncHandler (async (req,res)=>{
+  const {oldPassword, newPassword} = req.body
+  const user = await User.findById(req.user?._id)
+  const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+  if(!isPasswordCorrect){
+    throw new ApiError(400, "Invalid Old Password")
+  }
+    user.password = newPassword
+    await user.save({validateBeforeSave : false})
 
-export {registerUser, loginUser,logoutUser,refreshAccessToken}; 
+    return res
+    .status(200)
+    .json(
+      new ApiResponse(200, {}, "Password Changed Successfully")
+    )
+  
+})
+
+const getCurrentUser = asyncHandler ( async (req,res) =>{
+  return res
+  .status(200)
+  .json(
+    ApiResponse(200,req.user,"Current user fetched successfully")
+  )
+})
+
+const updateAccountDetails = asyncHandler ( async (req,res)=>{
+  const {fullName,email} = req.body
+  if(!fullName || !email){
+    throw new ApiError(400, "both email and fullName is required")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      fullName,// both fullName Or fullName: fullName are correct
+      email: email, //similar to email,
+    },
+    {new: true}//new true means update info will be returned and stored in the "user"
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json (new ApiResponse(200, user, "Account Details updated successfully"))
+})
+
+//now we're gonna update file(avatar and coverimage )changes for this purpose we'll have to use to middleware in the route first is use multer for file upload and second is verifyJWT for checking if the user is logged in or not Lets write
+const updateUserAvatar = asyncHandler(async(req,res)=>{
+  const avatarLocalPath = req.file?.path // this will get the path of avatar using multer middleware 
+  if(!avatarLocalPath){
+    throw new ApiError(400, "Avatar file is missing ")
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath)
+
+  if(!avatar.url){
+    throw new ApiError(400, "Error while uplaoding avatar on cloudinary ")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      avatar: avatar.url
+    },
+    {new: true}
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,user, "Avatar Uploaded Successfully")
+  )
+})
+
+const updateUserCoverImage = asyncHandler(async(req,res)=>{
+  const coverImageLocalPath = req.file?.path // this will get the path of avatar using multer middleware 
+  if(!coverImageLocalPath){
+    throw new ApiError(400, "CoverImage file is missing ")
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+  if(!coverImage.url){
+    throw new ApiError(400, "Error while uplaoding coverImage on cloudinary ")
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      coverImage: coverImage.url
+    },
+    {new: true}
+  ).select("-password")
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(200,user, "Cover Image Updated Successfully")
+  )
+})
+
+export {registerUser, loginUser,logoutUser,refreshAccessToken,changeCurrentPassword,getCurrentUser,updateUserAvatar,updateUserCoverImage }; 
